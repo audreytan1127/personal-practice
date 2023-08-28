6c53c90610a61bbf59af3d243e993a1f3c3070d9
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-27 22:52:09 -0500

    second algorithm question: sum of two numbers

---
+++ hackerrank/src/SolveMeFirst.java
@@ -0,0 +1,10 @@
+public class SolveMeFirst {
+
+    static int sumOfTwo(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfTwo(5, 4));
+    }
+}
